43c8a9d260be1dec7a05fe36d754abc909600fed
Gergely Szabo <gergely_sandor_szabo@epam.com>
2023-09-25 16:20:36 +0200

    OrderService modified to send orders to Service Bus as well.

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
+using Azure.Messaging.ServiceBus;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
@@ -52,6 +53,9 @@ public class OrderService : IOrderService
             return orderItem;
         }).ToList();
 
+        await using var client = new ServiceBusClient("Endpoint=sb://eshoponweborders.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Fh/ahpk6p8SDC1SM6+R6PEHLI8qg2bn7L+ASbI0dSf8=");
+        var sender = client.CreateSender("orders");
+
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         var itemsToOrder =
@@ -70,6 +74,9 @@ public class OrderService : IOrderService
 
         var json = JsonExtensions.ToJson(itemsToOrder);
         var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var busMessage = new ServiceBusMessage(json);
+        await sender.SendMessageAsync(busMessage);
 
         var response = await _httpClient.PostAsync("https://deliveryorderprocessorfunc.azurewebsites.net/api/Function1?code=2hWjSkjLhH84uJA_UUXo2jpxF8Y9kGpkAndGuXzrWzT3AzFuhn3rVw==", content);
 
